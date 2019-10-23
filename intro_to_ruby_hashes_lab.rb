36d18e88cb069a95398d8093a73e739e794ec018
Luana Faria <luanasfff@gmail.com>
2019-10-23 23:24:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -49,8 +49,9 @@ def the monopoly_with_fourth_tier
       {
         :reading_railroad => 
          {
-           :mortgage_value => "$100"
-         },
+           :mortgage_value => ("$100")
+         }
+  
         :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
         :shortline_railroad => {}
