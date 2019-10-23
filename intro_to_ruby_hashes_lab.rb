95496af0454c600c1b941a7266d743328eed4a7d
Luana Faria <luanasfff@gmail.com>
2019-10-23 23:10:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,8 +25,10 @@ def monopoly_with_third_tier
     :names => 
       {
         :reading_railroad => {},
-        :pennsylvania_railroad => {}
+        :pennsylvania_railroad => {},
+        :b_and_o_railroad => {},
         :
+        
       }
       
     }
